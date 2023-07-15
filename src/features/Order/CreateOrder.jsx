ac85b8b84affcf939cb15ffdc09d05e198a7eb21
RydKrm <alriyadkarim@gmail.com>
2023-07-15 11:24:59 +0600

    Finish but not

--- src/features/Order/CreateOrder.jsx
+++ src/features/Order/CreateOrder.jsx
@@ -1,11 +1,11 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../Services/apiRestaurant";
 import Button from "../../Ui/Button";
-import { useSelector } from "react-redux";
-import { clearCart, getCart, getTotalCartPrice } from "../Cart/CartSlice";
-import store from "../../store";
+import { useDispatch, useSelector } from "react-redux";
+import { getCart, getTotalCartPrice } from "../Cart/CartSlice";
 import { formatCurrency } from "../../Utilits/helpers";
 import { useState } from "react";
+import { fetchAddress } from "../User/userSlice";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -15,6 +15,17 @@ const isValidPhone = (str) =>
 
 function CreateOrder() {
   const [withPriority, setWithPriority] = useState(false);
+
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+    error: errorAddress,
+  } = useSelector((state) => state.user);
+  const isLoadingAddress = addressStatus === 'loading';
+ 
+  const dispatch = useDispatch();
   const cart = useSelector(getCart);
   const navigation = useNavigation();
   const isSubmitting = navigation.state==='submitting';
@@ -26,8 +37,6 @@ function CreateOrder() {
   return (
  <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
-
-      {/* <Form method="POST" action="/order/new"> */}
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
@@ -56,6 +65,10 @@ function CreateOrder() {
               required
             />
           </div>
+          <Button type="small" className="border-2 border-slate-800 p-3" onClick={(e)=>{
+           e.preventDefault();
+           dispatch(fetchAddress());
+          }} >Get position</Button>
         </div>
 
         <div className="mb-12 flex items-center gap-5">
